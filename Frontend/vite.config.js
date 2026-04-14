b53701396fd9007743728d514877b927ab839a39
dharmapal25 <dharmapalbharati2525@gmail.com>
2026-04-15 04:26:34 +0530

    frontend PWA create

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Flash Chat App',
+        short_name: 'ChatApp',
+        description: 'PWA Chat App',
+        theme_color: '#000000',
+        background_color: '#272727ff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/Flash.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/Flash.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
